bf625181f26e450c1f66936f9b7ea1999bb5ca1d
Deeparagavi <Deeparagavi1326@gmail.com>
2020-04-12 15:10:14 +0530

    networking

---
+++ day11-networking/include/common/graphicscontext.h
@@ -0,0 +1,14 @@
+#ifndef __MYOS_COMMON_GRAPHICSCONTEXT_H
+#define __MYOS_COMMON_GRAPHICSCONTEXT_H
+
+#include<drivers/vga.h>
+
+namespace myos
+{
+    namespace common
+    {
+        typedef myos::drivers::VideoGraphicsArray GraphicsContext ;
+    }
+}
+
+#endif
